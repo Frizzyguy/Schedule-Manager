eb947b5f6464f0023ea546ce684cb383a7be5eb9
faggot-hunter <caberoypaul16@gmail.com>
2026-06-07 14:30:06 +0800

    Update functions.hpp

--- include/functions.hpp
+++ include/functions.hpp
@@ -1,6 +1,37 @@
+#ifndef SCHEDULECLASS_HPP
+#define SCHEDULECLASS_HPP
+
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void printCentered(string text, int width);
+class schedule {
+public:
+    schedule(string n, string desc, int month, int day, string p);
 
-void printLine(int width);
+    string name;
+    string description;
+    string date;
+    string priority;
+    string status;
+};
+
+class manager {
+public:
+    manager();
+
+    void addTask(string n, string desc, int month, int day, string priority);
+    void showTaskTable(int width, int margin);
+
+    void deleteTask(int index);
+    void editTask(int index);
+
+    void markComplete(int index);
+    void filterByPriority(string p);
+    void filterByStatus(string s);
+
+private:
+    vector<schedule> tasks;
+};
+
+#endif
