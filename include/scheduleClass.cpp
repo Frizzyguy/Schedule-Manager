e2c309bbebf72db5293bd69b494f8286a0c82e67
faggot-hunter <caberoypaul16@gmail.com>
2026-06-07 14:30:49 +0800

    Update scheduleClass.cpp

--- include/scheduleClass.cpp
+++ include/scheduleClass.cpp
@@ -1,91 +1,110 @@
 #include "scheduleClass.hpp"
-#include "functions.hpp"
-#include <vector>
-#include <iostream>
-using namespace std;
 
-
-#pragma region //schedule function definition
-schedule::schedule(string n, string desc, int month, int day){
-            name = n;
-            description = desc;
-            date = to_string(month) + "/" + to_string(day);
+// constructor
+schedule::schedule(string n, string desc, int month, int day, string p){
+    name = n;
+    description = desc;
+    date = to_string(month) + "/" + to_string(day);
+    priority = p;
+    status = "Pending";
 }
-#pragma endregion
 
-
-#pragma region //manager function definition
+// manager constructor
 manager::manager(){}
-void manager::addTask(string n, string desc, int month, int day){
-    int textSize = tasks.size();
-    tasks.resize(textSize + 1, schedule(n,desc,month,day));
+
+// add task
+void manager::addTask(string n, string desc, int month, int day, string priority){
+    tasks.push_back(schedule(n, desc, month, day, priority));
 }
 
-
+// display tasks
 void manager::showTaskTable(int width, int margin){
-    int numberSection = 3;
-    string sectionText[numberSection] = {"Task Name","Description","Deadline"};
+    cout << "\n===== TASK LIST =====\n";
+    cout << "Index | Name | Description | Deadline | Priority | Status\n";
 
-    int sectionTextCharacterTotal = 0;
-    for (int i = 0; i < numberSection; i++){
-        sectionTextCharacterTotal += sectionText[i].size();
-    }
-
-    cout<<"\n";
-    printLine(width);
-    cout<<"\n";
-    printCentered("TASK TABLE",width);
-    cout<<"\n";
-    printLine(width);
-    
-    
-    int sectionWidth = (width % numberSection == 0) ? width/numberSection : (width/numberSection) + 1;
-    for (int i = 0; i < numberSection; i++){
-        int sectionPadding = ((sectionWidth - sectionText[i].size())/2);
-        string offsetPadding = ((sectionWidth - sectionText[i].size()) % 2) ? " " : ""; 
-        string padding = "";
-        while (sectionPadding - 1 != 0)
-        {
-            sectionPadding--;
-            padding += " ";
-        }
-        
-        string sectionBreak = " ";
-        if (i + 1 != numberSection){
-            sectionBreak = "|";
-        }
-        cout<< padding << sectionText[i] << padding << offsetPadding << sectionBreak;
-    }
-
-    cout<<"\n";
-    printLine(width);
-
-    int numOfTask = tasks.size();
-    for (int i = 0; i < numOfTask; i++){
-        string taskText[numberSection] = {};
-        taskText[0] = tasks[i].name;
-        taskText[1] = tasks[i].description;
-        taskText[2] = tasks[i].date;
-        
-        for (int j = 0; j < numberSection; j++){
-            int sectionPadding = (sectionWidth - taskText[j].size())/2;
-            string offsetPadding = ((sectionWidth - taskText[j].size()) % 2) ? " " : "";
-
-            string padding = "";
-            while (sectionPadding - 1 != 0){
-                sectionPadding--;
-                padding += " ";
-            }
-
-            string sectionBreak = " ";
-            if (j + 1 != numberSection){
-                sectionBreak = "|";
-            }
-            cout<< padding << taskText[j] << padding << offsetPadding << sectionBreak;
-        }
-
-        cout<<"\n";
+    for (int i = 0; i < tasks.size(); i++){
+        cout << i << " | "
+             << tasks[i].name << " | "
+             << tasks[i].description << " | "
+             << tasks[i].date << " | "
+             << tasks[i].priority << " | "
+             << tasks[i].status << "\n";
     }
 }
 
-#pragma endregion
+// delete
+void manager::deleteTask(int index){
+    if (index >= 0 && index < tasks.size()){
+        tasks.erase(tasks.begin() + index);
+        cout << "Task deleted!\n";
+    } else {
+        cout << "Invalid index!\n";
+    }
+}
+
+// edit
+void manager::editTask(int index){
+    if (index >= 0 && index < tasks.size()){
+        cin.ignore();
+
+        cout << "New name: ";
+        getline(cin, tasks[index].name);
+
+        cout << "New description: ";
+        getline(cin, tasks[index].description);
+
+        int m, d;
+        cout << "New month & day: ";
+        cin >> m >> d;
+        tasks[index].date = to_string(m) + "/" + to_string(d);
+
+        cout << "New priority (Low/Medium/High): ";
+        cin >> tasks[index].priority;
+
+        cout << "Task updated!\n";
+    } else {
+        cout << "Invalid index!\n";
+    }
+}
+
+// mark complete
+void manager::markComplete(int index){
+    if (index >= 0 && index < tasks.size()){
+        tasks[index].status = "Completed";
+        cout << "Task marked as completed!\n";
+    } else {
+        cout << "Invalid index!\n";
+    }
+}
+
+// filter priority
+void manager::filterByPriority(string p){
+    cout << "\nFiltered by Priority: " << p << "\n";
+
+    for (int i = 0; i < tasks.size(); i++){
+        if (tasks[i].priority == p){
+            cout << i << " | "
+                 << tasks[i].name << " | "
+                 << tasks[i].description << " | "
+                 << tasks[i].date << " | "
+                 << tasks[i].priority << " | "
+                 << tasks[i].status << "\n";
+        }
+    }
+}
+
+// filter status
+void manager::filterByStatus(string s){
+    cout << "\nFiltered by Status: " << s << "\n";
+
+    for (int i = 0; i < tasks.size(); i++){
+        if (tasks[i].status == s){
+            cout << i << " | "
+                 << tasks[i].name << " | "
+                 << tasks[i].description << " | "
+                 << tasks[i].date << " | "
+                 << tasks[i].priority << " | "
+                 << tasks[i].status << "\n";
+        }
+    }
+}
