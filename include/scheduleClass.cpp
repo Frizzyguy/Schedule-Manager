97badd148c991cb6ee9769e1e2079773b2299b1b
faggot-hunter <caberoypaul16@gmail.com>
2026-06-07 14:53:57 +0800

    Delete include directory

--- include/scheduleClass.cpp
+++
@@ -1,9 +0,0 @@
-#include "scheduleClass.hpp"
-
-Task::Task(string n, string d, int m, int day, string p) {
-    name = n;
-    description = d;
-    date = to_string(m) + "/" + to_string(day);
-    priority = p;
-    status = "Pending";
-}
