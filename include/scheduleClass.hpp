1f73c32c344c445e892be792403030cf504fac7b
faggot-hunter <caberoypaul16@gmail.com>
2026-06-07 14:36:18 +0800

    Update scheduleClass.hpp

--- include/scheduleClass.hpp
+++ include/scheduleClass.hpp
@@ -2,36 +2,17 @@
 #define SCHEDULECLASS_HPP
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
-class schedule {
+class Task {
 public:
-    schedule(string n, string desc, int month, int day, string p);
-
     string name;
     string description;
     string date;
     string priority;
     string status;
-};
 
-class manager {
-public:
-    manager();
-
-    void addTask(string n, string desc, int month, int day, string priority);
-    void showTaskTable(int width, int margin);
-
-    void deleteTask(int index);
-    void editTask(int index);
-
-    void markComplete(int index);
-    void filterByPriority(string p);
-    void filterByStatus(string s);
-
-private:
-    vector<schedule> tasks;
+    Task(string n, string d, int m, int day, string p);
 };
 
 #endif
